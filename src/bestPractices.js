ec9b4f2389904a40ed5fe44d10185a7032a228e4
Kaue Miyauti Machado <kaumac@gmail.com>
2016-09-25 21:19:34 -0300

    init front-end js test

---
+++ src/bestPractices.js
@@ -0,0 +1,23 @@
+/**
+ * This file defines an object with some methods. Some of these methods are
+ * populated incorrectly; your job is to fix them. Other methods are not
+ * populated at all; your job is to fill them out.
+ */
+
+module.exports = {
+    globals : function() {
+        myObject = {
+            name : 'Jory'
+        };
+
+        return myObject;
+    },
+
+    parseInt : function(num) {
+        return parseInt(num);
+    },
+
+    identity : function(val1, val2) {
+
+    }
+};
